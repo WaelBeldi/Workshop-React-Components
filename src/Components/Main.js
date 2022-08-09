26eda26eb4baac7e80eafeff8271b69fa37c0e35
WaelBeldi <waelbeldi1990@gmail.com>
2022-08-09 11:01:59 +0100

    React components

---
+++ src/Components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import MyCard from './MyCard'
+
+
+const Main = () => {
+  return (
+    <div className="main">
+        <MyCard />
+        <MyCard />
+        <MyCard />
+    </div>
+  )
+}
+
+export default Main
---
+++ src/Components/MyPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "./Footer";
+import Main from "./Main";
+import NavBar from "./NavBar";
+
+const MyPage = () => {
+  return (
+    <div>
+      <NavBar />
+      <Main />
+      <Footer />
+    </div>
+  );
+};
+
+export default MyPage;
